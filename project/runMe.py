cee6bb0383fa86abd271b72e98113c9d80070274
roy-pstr <roy.pasternakk@gmail.com>
2021-01-04 22:29:26 +0200

    fixed runme

--- project/runMe.py
+++ project/runMe.py
@@ -1,20 +1,4 @@
-import numpy as np
-from numpy import random
-import ast
 import os
-import torch
-from IPython.display import Image, clear_output  # to display images
-from models.experimental import attempt_load
-from utils.general import check_img_size, non_max_suppression, scale_coords, xyxy2xywh, increment_path
-from utils.torch_utils import select_device
-from utils.datasets import LoadImages
-from utils.plots import plot_one_box
-from pathlib import Path
-import cv2
-import time
-import matplotlib.pyplot as plt
-from torchvision import transforms
-from PIL import Image
 from detect import detect
 
 
